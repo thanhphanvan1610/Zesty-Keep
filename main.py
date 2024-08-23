548ae538e6553b579daee837571ff0292601e43f
Phan Văn Thành <thanhphanvan1610@gmail.com>
2024-08-23 00:41:13 -0700

    update: telegram bot completion

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import os
-import schedule
-import time
-import threading
 import asyncio
 from utils.reset_new_vocabulary import new_vocabulary
 from platforms.telegram_bot import TelegramBot
 from dotenv import load_dotenv
 from database.database import initialize_db
 from utils.logger import setup_logging
-import logging
 
 setup_logging()
 
@@ -17,16 +13,6 @@ load_dotenv()
 bot_token = os.getenv("TELEBOT_API") or os.environ.get("TELEBOT_API")
 note_id = "19178354b1a.d1c0431359128b0d"
 
-def start_scheduler(note_id):
-    """Function to start the scheduler."""
-    schedule.every().day.at("07:00").do(lambda: new_vocabulary(note_id))
-    logging.info("Scheduler started. Waiting for the next run...")
-    try:
-        while True:
-            schedule.run_pending()
-            time.sleep(60)
-    except KeyboardInterrupt:
-        logging.info("Scheduler stopped by user.")
     
 
 def run_bot(bot_token, note_id):
@@ -39,14 +25,7 @@ def run_bot(bot_token, note_id):
 def main():
     # Initialize the database in the main thread
     initialize_db()
-
-    # Start the Telegram bot in a separate thread
-    bot_thread = threading.Thread(target=run_bot, args=(bot_token, note_id))
-    bot_thread.daemon = True
-    bot_thread.start()
-
-    # Start the scheduler in the main thread
-    start_scheduler(note_id)
-
+    run_bot(bot_token, note_id)
+    
 if __name__ == "__main__":
     main()
